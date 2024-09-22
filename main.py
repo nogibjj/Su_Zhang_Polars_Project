41b506f692fe1ea21db1c5650cc8a50090e2c5c0
张塑 <zhangsu@zhangsudeMBP.lan>
2024-09-22 01:51:28 -0400

    Edited

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 
-# Load the healthcare dataset
-health_df = pd.read_csv("healthcare_dataset.csv")
+dataframe = None
 
 
-# print the head of the datasets to showcase what variables do the dataset includes
-def print_head():
-    health_head = health_df.head()
-    print(health_head)
-    return health_head
+# Load dataset from csv file using Polars
+def load_dataset_pl(dataset):
+    df = pl.read_csv(dataset)
+    return df
 
 
-# describe the dataset's discrete / continuous variables
-def describe_stat():
-    health_desc = health_df.describe()
-    print(health_desc)
-    return health_desc
+# print the head of the datasets to showcase what variables do the dataset include
+def print_head(df):
+    data_head = df.head()
+    print(data_head)
+    return data_head
+
+
+# describe the dataset's specific columns
+def describe_stat(df, col_list):
+    return df.select(col_list).describe()
 
 
 # use value counts to summary the counts of categorical variables
-def group_by(column_name):
-    return health_df[column_name].value_counts()
+def group_by(df, column_name):
+    return df.select(pl.col(column_name).value_counts())
 
 
 # create a histogram of 'Billing Amount' column in healthcare dataset
-def build_histogram():
+def build_histogram(df):
     plt.hist(
-        health_df["Billing Amount"],
+        df["Billing Amount"],
         bins=20,
         edgecolor="white",
     )
@@ -38,12 +41,27 @@ def build_histogram():
     plt.show()
 
 
+def save_to_markdown():
+    describe_df = describe_stat(dataframe, ["Age", "Billing Amount"])
+    markdown_table = str(describe_df)
+    with open("healthcare_summary.md", "w", encoding="utf-8") as file:
+        file.write("Healthcare Dataset Describe:\n")
+        file.write(markdown_table)
+        file.write("\nHistogram of Billing Amount Distribution\n")
+        file.write("![Graph Description](Billing_Amount_Hist.png)")
+
+
 # main to execute the functions.
 if __name__ == "__main__":
-    print_head()
-    describe_stat()
-    print(group_by("Blood Type"))
-    print(group_by("Medical Condition"))
-    print(group_by("Admission Type"))
-    print(group_by("Test Results"))
-    build_histogram()
+    dataframe = load_dataset_pl("healthcare_dataset.csv")
+    if dataframe is not None:
+        print_head(dataframe)
+        print(describe_stat(dataframe, ["Age", "Billing Amount"]))
+        print(group_by(dataframe, "Blood Type"))
+        print(group_by(dataframe, "Medical Condition"))
+        print(group_by(dataframe, "Admission Type"))
+        print(group_by(dataframe, "Test Results"))
+        build_histogram(dataframe)
+        save_to_markdown()
+    else:
+        print("Dataset was not successfully loaded")
--- test_main.py
+++ test_main.py
@@ -1,22 +1,54 @@
-from main import describe_stat, group_by, build_histogram
+from main import load_dataset_pl, describe_stat, group_by, build_histogram
+import pandas as pd
+from pyinstrument import Profiler
+
+data = load_dataset_pl("healthcare_dataset.csv")
 
 
 def test_describe_stat():
     """testing out if describe_stat function works"""
-    output = describe_stat()
+    output = describe_stat(data, ["Age", "Billing Amount"])
     assert output is not None
 
 
 def test_group_by():
     """testing out if group_by function works"""
-    output_grouby = group_by("Blood Type")
+    output_grouby = group_by(data, "Blood Type")
     assert output_grouby is not None
 
 
 def test_build_histogram():
     """testing out if build_histogram works"""
-    output_hist = build_histogram()
+    output_hist = build_histogram(data)
     assert output_hist is None
+
+
+# Define loading dataset function using pandas:
+def load_dataset_pd(dataset):
+    df_pd = pd.read_csv(dataset)
+    return df_pd
+
+
+# Use Profiler to compare pandas and polars
+with Profiler(interval=0.1) as profiler:
+    check_pl = "healthcare_dataset.csv"
+    df = load_dataset_pl(check_pl)
+    print(df.shape)
+    print(df["Age"].mean())
+    print(df["Age"].median())
+
+print("-------Below is summary profile for Polars--------")
+profiler.print()
+
+with Profiler(interval=0.1) as profiler:
+    check_pd = "healthcare_dataset.csv"
+    df = load_dataset_pd(check_pd)
+    print(df.shape)
+    print(df["Age"].mean())
+    print(df["Age"].median())
+
+print("-------Below is summary profile for Pandas--------")
+profiler.print()
 
 
 if __name__ == "__main__":
